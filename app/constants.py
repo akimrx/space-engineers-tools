260f7cc9fe576609f7a3db4a5d3cbf35e87c27fc
Аким Фасхутдинов <a.faskhutdinov@yclients.tech>
2020-12-18 05:02:27 +0300

    Oh, I forgot that we work with windows. But nothing, everything is fixable.

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,4 @@
-SAVES = r"C:\space-engineers\torch-server\Instance\Saves"
+SAVES = r"C:\space-engineers\torch-server\Instance\Saves\bad-engineers\Backup"
 BUCKET = "space-engineers"
 ENDPOINT = "storage.yandexcloud.net"
 REGION = "us-east-1"
