5c5417231d508901466f43ea77aac29151b7efa5
akimrx <akimstrong@yandex.ru>
2020-12-19 15:12:13 +0300

    fixed server location

--- app/constants.py
+++ app/constants.py
@@ -1,7 +1,9 @@
 import os
 
-SAVES = os.environ.get("SE_SAVES_PATH") or r"C:\space-engineers\torch-server\Instance\Saves\bad-engineers\Backup"
-PREFIX = "backups"
+BASEDIR = r"C:\Users\Administrator\AppData\Roaming\SpaceEngineersDedicated\"
+WORLD_NAME = os.environ.get("SE_WORLD_NAME") or r"Star System"
+SAVES = os.environ.get("SE_SAVES_PATH") or r"{0}\Saves\{1}\Backup".format(BASEDIR, WORLD_NAME)
+PREFIX =  "backups"
 BUCKET = "space-engineers"
 ENDPOINT = "https://storage.yandexcloud.net"
 REGION = "us-east-1"
