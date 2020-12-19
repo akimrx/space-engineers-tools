7f96dab6079f98b662a72790c5858c0bafb36c7c
akimrx <akimstrong@yandex.ru>
2020-12-19 15:12:29 +0300

    fixed server location

--- app/constants.py
+++ app/constants.py
@@ -1,6 +1,6 @@
 import os
 
-BASEDIR = r"C:\Users\Administrator\AppData\Roaming\SpaceEngineersDedicated\"
+BASEDIR = r"C:\Users\Administrator\AppData\Roaming\SpaceEngineersDedicated"
 WORLD_NAME = os.environ.get("SE_WORLD_NAME") or r"Star System"
 SAVES = os.environ.get("SE_SAVES_PATH") or r"{0}\Saves\{1}\Backup".format(BASEDIR, WORLD_NAME)
 PREFIX =  "backups"
